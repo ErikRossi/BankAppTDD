b73e6985c23b2042e53a608fdcdba65f48a7403e
Erik Rossi <>
2017-01-26 08:23:36 +0100

    -Commented code. -Added multiple tests for withdraw. -TO-DO list integrated in BankAccount.java.

--- src/bankaccount/BankAccount.java
+++ src/bankaccount/BankAccount.java
@@ -1,25 +1,60 @@
 package bankaccount;
 
+//--------TO-DO
+//-Introduce MAVEN support.
+//-Write error messages for every state.
+//-Show error message to user, so they know what happened why it error.
+//
+
+
 public class BankAccount {
 
     private double balance;
     
-    public double returnBalance() {
-        return balance;
+    public double getBalance(BankAccount ba) {
+        return ba.balance;
     }
-
-    public void deposit(int i) {
+    
+    //Method for testing purposes only.
+    public void setBalance(BankAccount ba, int i) {
+        balance = i;
+    }
+    
+    
+    //Functionality for depositing money.
+    public void deposit(BankAccount ba, int i) {
         //Makes sure you can't deposit negative amounts.
         if (i < 0) {
             throw new IllegalArgumentException();
         }
+        //Successful deposit.
         else {
-        balance += i;
+            balance += i;
         }
     }
 
-    public void withdraw(int i) {
-        balance -= i;
+    //Functionality for withdrawing money.
+    public void withdraw(BankAccount ba, int i) {     
+        //Can't withdraw if requested amount is negative.
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
+        //Can't withdraw if balance is less than or equal to zero.
+        if (ba.getBalance(ba) <= 0) {
+            throw new ArithmeticException();
+        }
+        //Cannot withdraw amounts that exceeds balance of -100.
+        if (ba.getBalance(ba) - i < -100) {
+            throw new IllegalStateException();
+        }
+        //Successful withdrawal.
+        else {
+            ba.balance -= i;
+        }
+        //Five dollar penalty if customer overdraws.
+        if (ba.balance < 0) {
+            ba.balance -= 5;
+        }
     }
     
     
--- test/testbankaccount/TestBankAccount.java
+++ test/testbankaccount/TestBankAccount.java
@@ -13,51 +13,64 @@ public class TestBankAccount {
     public TestBankAccount() {
     }
     
+    //Sets ba's balance to 0.
     @Before
     public void setUp() {
-        ba.deposit(0);
+        ba.deposit(ba,0);
     }
 
-//    @Test
-//    public void testReturnBalance() {
-//        assertEquals(500, ba.returnBalance(), 0);
-//    }
-    
+    //Test standard, run-of-the-mill deposits.
     @Test
     public void testDeposit() {
-        
-        assertEquals(0,ba.returnBalance(),0);
-        ba.deposit(500);
-        assertEquals(500,ba.returnBalance(),0);
+        assertEquals(0,ba.getBalance(ba),0);
+        ba.deposit(ba, 500);
+        assertEquals(500,ba.getBalance(ba),0);
     }
     
-    @Test
-    public void testWithdraw() {
-        ba.deposit(500);
-        assertEquals(500,ba.returnBalance(),0);
-        ba.withdraw(300);
-        assertEquals(200, ba.returnBalance(),0);
-    }
-    
+    //Disallows deposits of negative amounts. To prevent typos.
     @Test(expected=IllegalArgumentException.class)
     public void testDepositWithNegativeAmount() {
-        ba.deposit(-100);
+        ba.deposit(ba, -100);
+    }    
+    
+    //Tests whether standard withdrawal functionality works.
+    @Test
+    public void testWithdraw() {
+        ba.deposit(ba, 500);
+        assertEquals(500,ba.getBalance(ba),0);
+        ba.withdraw(ba, 300);
+        assertEquals(200, ba.getBalance(ba),0);
+    }
+    
+    //Makes sure negative amounts in code doesn't work.
+    @Test (expected=IllegalArgumentException.class)
+    public void testWithdrawWithNegativeAmount() {
+        ba.withdraw(ba, -100);        
     }
     
     //Test disallow withdrawal when balance is negative.
-    @Test
+    @Test (expected = ArithmeticException.class)
     public void testWithdrawWhenBalanceIsNegative() {
-        
+        ba.setBalance(ba, -100);
+        assertEquals(-100, ba.getBalance(ba), 0);
+        ba.withdraw(ba, 100);
     }    
+    
     //Gives a -5 dollar penalty if customer withdraws too much.
     @Test
     public void testBalanceAfterPenaltyIsIncurred() {
-        
+        ba.withdraw(ba, 100);
+        assertEquals(-105, ba.getBalance(ba), 0);
     }
     
     //Test disallowance of withdrawal if user tries to go over -100 balance.
-    @Test
-    public void testWithdrawMaximumNegativeAmount() {
-        
+    //Withdrawal is thereafter cancelled.
+    @Test (expected=IllegalStateException.class)
+    public void testWithdrawMaximumNegativeAmount() { 
+        ba.deposit(ba, 1);
+        assertEquals(1, ba.getBalance(ba), 0);
+        ba.withdraw(ba, 200);
+        assertEquals(1, ba.getBalance(ba), 0);
     }
+    
 }
